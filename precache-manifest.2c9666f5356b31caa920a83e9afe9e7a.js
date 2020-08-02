937878499585deb8116f6232f991a1cf79c3d5ab
Patryk <matasinqwer@gmail.com>
2020-08-02 18:32:29 +0200

    Updates

---
+++ precache-manifest.2c9666f5356b31caa920a83e9afe9e7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75cf7a9b3c980cee0350f1bf2c5b5d3d",
+    "url": "/harry-potter-app/index.html"
+  },
+  {
+    "revision": "b43f1a7404d010152111",
+    "url": "/harry-potter-app/static/css/main.f70a7f9e.chunk.css"
+  },
+  {
+    "revision": "6ffd02a2673b48bd88ee",
+    "url": "/harry-potter-app/static/js/2.5d184b49.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/harry-potter-app/static/js/2.5d184b49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43f1a7404d010152111",
+    "url": "/harry-potter-app/static/js/main.06e869e2.chunk.js"
+  },
+  {
+    "revision": "81447f66ad8d31bbf76e",
+    "url": "/harry-potter-app/static/js/runtime-main.116cfd0c.js"
+  }
+]);
